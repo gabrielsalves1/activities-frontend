513469fedf53423144dbf3ca92debae6ece003a1
gabrielsalves1 <gabrielsalves2k18@gmail.com>
2022-04-02 19:28:59 -0300

    Ajusta Upload de arquivos com formData

--- src/components/Formulary/index.jsx
+++ src/components/Formulary/index.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import api from '../../services/api';
+import axios from 'axios';
 import { useForm } from "react-hook-form";
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Image } from 'react-bootstrap';
 import style from './Formulary.module.scss';
 
 export default function Formulary() {
   const { register, handleSubmit, formState: { errors } } = useForm();
+  const [ file, setFile ] = useState();
+  const [ baseImage, setBaseImage ] = useState();
 
-  const onSubmit = data => { 
-    console.log(typeof data.file[0])
-    api.post("/activities", data)
+  const handleFile = (e) => {
+    setFile(e.target.files[0])
+  }
+
+  const onSubmit = data => {
+    let formData = new FormData();
+    formData.append('activityFile', file)
+    formData.append('name', data['name'])
+    formData.append('description', data['description'])
+    formData.append('date', data['date'])
+
+    api.post('http://127.0.0.1:3001/api/v1/activities', formData)
+    .then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    })
   }
 
   return (
@@ -31,12 +48,16 @@ export default function Formulary() {
         {errors.date && <span>Esse campo é obrigatório.</span>}
       </Form.Group>
 
-      <Form.Group controlId="formFile" className="mb-3" htmlFor='file'>
+      <Form.Group controlId="formFile" className="mb-3" htmlFor='activityFile'>
         <Form.Label>Arquivo</Form.Label>
-        <Form.Control type="file" name="file" {...register("file", { required: true })} />
+        <Form.Control type="file" name="activityFile" onChange={(e) => {
+          handleFile(e)
+        }} />
         {errors.file && <span>Esse campo é obrigatório.</span>}
       </Form.Group>
-
+      
+      <Image src={baseImage} rounded />
+      <br/>
       <Button type="submit" value='Enviar' className={style.botaoEnviar}>Enviar</Button>
     </Form>
   );
